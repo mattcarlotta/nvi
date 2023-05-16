b5b13717addf37ef3e134106afb1f27cfffafcc5
Matt <matt@mattcarlotta.com>
2023-05-15 17:27:44 -0700

    feat(reader): reduce some WET code and add comments

--- src/log.ts
+++ src/log.ts
@@ -4,16 +4,7 @@
  * @param message
  */
 export function logInfo(message: string, file?: string, line?: number): void {
-    console.log(`\x1b[32m[nvi${file && line ? `::${file}::${line}` : ''}] INFO: ${message}\x1b[0m`);
-}
-
-/**
- * A utility function that logs a debug message.
- *
- * @param message
- */
-export function logMessage(message: string): void {
-    console.log(`\x1b[90m[nvi]: ${message}\x1b[0m`);
+    console.log(`\x1b[32m[nvi${file ? `::${file}` : ''}${line ? `::${line}` : ''}] INFO: ${message}\x1b[0m`);
 }
 
 /**
@@ -23,7 +14,7 @@ export function logMessage(message: string): void {
  */
 export function logWarning(message: string, file?: string, line?: number): void {
     console.log(
-        `\x1b[33m[nvi${file && line ? `::${file}::${line}` : ''}] WARNING: ${message}\x1b[0m`
+        `\x1b[33m[nvi${file ? `::${file}` : ''}${line ? `::${line}` : ''}] WARNING: ${message}\x1b[0m`
     );
 }
 
@@ -35,7 +26,7 @@ export function logWarning(message: string, file?: string, line?: number): void 
  */
 export function logError(message: string, file?: string, line?: number): void {
     console.log(
-        `\x1b[31m[nvi${file && line ? `::${file}::${line}` : ''}] ERROR: ${message}\x1b[0m`
+        `\x1b[31m[nvi${file ? `::${file}` : ''}${line ? `::${line}` : ''}] ERROR: ${message}\x1b[0m`
     );
     if (process.env.NODE_ENV !== 'test') process.exit(1);
 }
--- src/readEnvFile.ts
+++ src/readEnvFile.ts
@@ -2,12 +2,11 @@ import type { ConfigOptions, ParsedEnvs } from './index';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import process from 'process';
-// import { logError, logInfo } from './log';
 import { logError, logInfo, logWarning } from './log';
 
 const COMMENT = 0x23;
 const LINE_DELIMITER = 0x0a;
-const ESC = 0x5c;
+const BACK_SLASH = 0x5c;
 const ASSIGN_OP = 0x3d;
 const DOLLAR_SIGN = 0x24;
 const OPEN_BRACE = 0x7b;
@@ -16,7 +15,7 @@ const CLOSE_BRACE = 0x7d;
 /**
  * Reads an ".env" file and parses keys and values into a Map object
  *
- * @param fileName - the name of the ".env.*" file (without a path)
+ * @param fileName - the name of the ".env" file (without a path)
  * @param options - an option object of args: { `debug`: boolean | string, `dir`: string, `encoding`: BufferEncoding, `envMap`: Map<string, string> | undefined, `override`: boolean | string | undefined, }
  * @returns a single {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map | `Map`} object of Envs
  * @example readEnvFile(".env", { dir: "example", debug: false, encoding: "utf-8", envMap: new Map(), override: true });
@@ -30,47 +29,38 @@ export default function readEnvFile(fileName: string, options = {} as ConfigOpti
         let byteCount = 0;
         let lineCount = 0;
         while (byteCount < fileBuf.length) {
+
             // skip lines that begin with comments
             const lineBuf = fileBuf.subarray(byteCount, fileBuf.length);
+            let lineDelimiterIndex = lineBuf.indexOf(LINE_DELIMITER);
             if (lineBuf[0] === COMMENT || lineBuf[0] === LINE_DELIMITER) {
                 ++lineCount;
-                const firstNewLineByte = fileBuf
-                    .subarray(byteCount, fileBuf.length)
-                    .indexOf(LINE_DELIMITER);
-
-                if (firstNewLineByte >= 0) {
-                    byteCount += firstNewLineByte + 1;
-                    continue;
-                } else {
-                    // this shouldn't ever throw, but here just in case
-                    throw Error('Found a comment or line delimiter, but was unable to locate the end of the current line!');
-                }
+                byteCount += lineDelimiterIndex + 1;
+                continue;
             }
 
-            let lineDelimiterIndex = lineBuf.indexOf(LINE_DELIMITER);
+            // check that there's still a new line delimiter
             if (lineDelimiterIndex >= 0) {
                 ++lineCount;
 
-                // check if multi-line value and find the end of the line
-                if (lineBuf[lineDelimiterIndex - 1] === ESC && lineBuf[lineDelimiterIndex] === LINE_DELIMITER) {
-                    while (lineBuf[lineDelimiterIndex - 1] === ESC) {
-                        ++lineCount;
-                        const nextLineDelimiterIndex = lineBuf.subarray(lineDelimiterIndex + 1, lineBuf.length).indexOf(LINE_DELIMITER);
-                        if (nextLineDelimiterIndex >= 0) {
-                            lineDelimiterIndex += nextLineDelimiterIndex + 1;
-                        } else {
-                            // this shouldn't ever throw, but here just in case
-                            throw Error('Found a multi-line value but was unable to locate the end of the current line!');
-                        }
+                // check if there's a multi-line value and locate the end, where the end is the first line with a non-delineated '\'
+                while (lineBuf[lineDelimiterIndex - 1] === BACK_SLASH) {
+                    ++lineCount;
+
+                    const nextLineDelimiterIndex = lineBuf.subarray(lineDelimiterIndex + 1, lineBuf.length).indexOf(LINE_DELIMITER);
+                    if (nextLineDelimiterIndex >= 0) {
+                        lineDelimiterIndex += nextLineDelimiterIndex + 1;
+                    } else {
+                        logError(`Found a multi-line value here: '${lineBuf.toString().split("\n").join("")}', but was unable to locate the end of the value.\n\nFor example, a multi-line value should end with a non-delineated ('\\') value:\nMULTI_LINE_KEY=123\\\n456\\\n789\nNEXT_KEY=abc\n`, fileName, lineCount);
                     }
                 }
 
-                // check for assignment '=' to split key from value
+                // check for an assignment '=' to split key from value
                 const assignIndex = lineBuf.indexOf(ASSIGN_OP);
                 if (assignIndex >= 0) {
                     const key = lineBuf.subarray(0, assignIndex).toString(options.encoding);
 
-                    // skip reading from process.env if override wasn't set
+                    // skip writing to process.env if override wasn't set
                     if (process.env[key] && !options.override) {
                         byteCount += lineDelimiterIndex + 1;
                         continue;
@@ -79,9 +69,10 @@ export default function readEnvFile(fileName: string, options = {} as ConfigOpti
                     let valBuf = lineBuf.subarray(assignIndex + 1, lineDelimiterIndex);
                     let valByteCount = 0;
                     while (valByteCount < valBuf.length) {
-                        // check if chunk contains multi-line breaks
+
+                        // check if chunk contains multi-line breaks and remove them from the buffer
                         let chunk = valBuf.subarray(valByteCount - 1, valByteCount + 1);
-                        if (chunk[0] === ESC && chunk[1] === LINE_DELIMITER) {
+                        if (chunk[0] === BACK_SLASH && chunk[1] === LINE_DELIMITER) {
                             const prevValBuf = valBuf.subarray(0, valByteCount - 1);
                             const afterValBuf = valBuf.subarray(valByteCount + 1, valBuf.length);
 
@@ -89,7 +80,7 @@ export default function readEnvFile(fileName: string, options = {} as ConfigOpti
                             valByteCount -= 2;
                         }
 
-                        // check if chunk contains an interpolated variable
+                        // check if chunk contains an interpolated key variable
                         chunk = valBuf.subarray(valByteCount, valByteCount + 2);
                         if (chunk[0] === DOLLAR_SIGN && chunk[1] === OPEN_BRACE) {
                             const valSliceBuf = valBuf.subarray(valByteCount, valBuf.length);
@@ -109,7 +100,7 @@ export default function readEnvFile(fileName: string, options = {} as ConfigOpti
                                     );
                                 }
 
-                                // replace interpolated variable bytes with the keyVal value
+                                // replace interpolated key variable with an interpolated value
                                 const prevBuf = valBuf.subarray(0, valByteCount);
                                 const newValBuf = Buffer.from(keyVal || '');
                                 const afterBuf = valBuf.subarray(
