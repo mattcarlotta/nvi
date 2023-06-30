fc88d5695fc386d1f676fd8e2efa4b4d4b828624
Matt Carlotta <matt@mattcarlotta.com>
2023-06-30 14:48:21 -0700

    feat(bin): simplify main to prioritze --config options

--- bin/src/main.cpp
+++ bin/src/main.cpp
@@ -2,29 +2,35 @@
 #include "json.cpp"
 #include "load_config.h"
 #include "parser.h"
+#include <optional>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
 
 int main(int argc, char *argv[]) {
     nvi::arg_parser arg(argc, argv);
+    vector<string> files = arg.files;
+    std::optional<string> dir = arg.dir;
+    bool debug = arg.debug;
+    vector<string> required_envs = arg.required_envs;
 
-    const auto env_config = arg.config;
-    if (env_config.length()) {
-        nvi::config config(env_config);
-        nvi::parser parser(config.get_dir(), config.get_debug().value());
-
-        for (const auto env : config.get_files()) {
-            parser.read(env)->parse();
-        }
-
-        parser.check_required(config.get_required_envs())->print();
-    } else {
-        nvi::parser parser(arg.dir, arg.debug);
-
-        for (const auto env : arg.files) {
-            parser.read(env)->parse();
-        }
-
-        parser.check_required(arg.required_envs)->print();
+    if (arg.config.length()) {
+        nvi::config config(arg.config);
+        dir = config.get_dir();
+        debug = config.get_debug().value();
+        files = config.get_files();
+        required_envs = config.get_required_envs();
     }
+
+    nvi::parser parser(dir, debug);
+
+    for (const auto env : files) {
+        parser.read(env)->parse();
+    }
+
+    parser.check_required(required_envs)->print();
 
     return 0;
 }
