74b6e5fe5111ff6e6196ef9b08f32bf4a7109259
Matt <matt@mattcarlotta.com>
2023-07-24 17:20:09 -0700

    fix: remove command desctructor from options

--- src/options.h
+++ src/options.h
@@ -12,11 +12,6 @@ namespace nvi {
             std::string dir;
             std::vector<std::string> files = std::vector<std::string>{".env"};
             std::vector<std::string> required_envs;
-            ~Options() {
-                for (char *command : commands) {
-                    delete[] command;
-                }
-            }
     };
 }; // namespace nvi
 
