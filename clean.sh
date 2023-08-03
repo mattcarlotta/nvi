d4e7c86110ba5965a3f24705420fc8aac87fd1f9
Matt <matt@mattcarlotta.com>
2023-08-03 13:09:46 -0700

    chore: update clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles/ install_manifest.txt Makefile
+rm -rf bin cmake_install.cmake CMakeCache.txt CMakeFiles/ CTestTestfile.cmake install_manifest.txt lib Makefile
 cd src
 rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles/ Makefile nvi
+cd ../tests
+rm -rf cmake_install.cmake CMakeFiles/ CTestTestfile.cmake libnvi_test.a Makefile tests
+
+
