0d5049651b8729e373482287000624cdd35f6d3f
Matt <matt@mattcarlotta.com>
2023-08-05 17:27:55 -0700

    chore(examples): update python example

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,11 +1,17 @@
 import os
 
-assert os.environ.get("BASIC_ENV") == "true"
-assert os.environ.get("QUOTES") == "sad\"wow\"bak"
-assert os.environ.get(
-    "MULTI_LINE_KEY") == "ssh-rsa BBBBPl1P1AD+jk/3Lf3Dw== test@example.com"
-assert os.environ.get(
-    "MONGOLAB_URI") == ("mongodb://root:password@abcd1234.mongolab.com:"
-                        "12345/localhost")
 
-print("Successfully assigned ENVs to the process!")
+def main():
+    assert os.environ.get("BASIC_ENV") == "true"
+    assert os.environ.get("QUOTES") == "sad\"wow\"bak"
+    assert os.environ.get(
+        "MULTI_LINE_KEY") == "ssh-rsa BBBBPl1P1AD+jk/3Lf3Dw== test@example.com"
+    assert os.environ.get(
+        "MONGOLAB_URI") == ("mongodb://root:password@abcd1234.mongolab.com:"
+                            "12345/localhost")
+
+    print("Successfully assigned ENVs to the process!")
+
+
+if __name__ == "__main__":
+    main()
