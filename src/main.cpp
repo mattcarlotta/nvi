b3021d7796d1233322525db869f4bddb9248aecf
Matt <matt@mattcarlotta.com>
2023-08-24 13:42:53 -0700

    chore: move env_map into generator within main

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
     Parser parser(std::move(tokens), options);
     env_map_t env_map = parser.parse_tokens()->check_envs()->get_env_map();
 
-    Generator generator(env_map, std::move(options));
+    Generator generator(std::move(env_map), std::move(options));
     generator.set_or_print_envs();
 
     std::exit(EXIT_SUCCESS);
