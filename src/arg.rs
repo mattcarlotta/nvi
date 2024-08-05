d9e9300f61ec02f444c959038c31734ecda4c148
Matt Carlotta <matt@mattcarlotta.com>
2024-08-05 15:32:57 -0700

    chore: minor optimizations to args

--- src/arg.rs
+++ src/arg.rs
@@ -42,32 +42,51 @@ impl ARG {
             ARG::UNKNOWN => "",
         }
     }
+
+    fn debug(&self) -> &'static str {
+        match self {
+            ARG::API => "API",
+            ARG::CONFIG => "CONFIG",
+            ARG::DEBUG => "DEBUG",
+            ARG::DIRECTORY => "DIRECTORY",
+            ARG::ENVIRONMENT => "ENVIRONMENT",
+            ARG::EXECUTE => "EXECUTE",
+            ARG::FILES => "FILES",
+            ARG::HELP => "HELP",
+            ARG::PRINT => "PRINT",
+            ARG::PROJECT => "PROJECT",
+            ARG::REQUIRED => "REQUIRED",
+            ARG::SAVE => "SAVE",
+            ARG::VERSION => "VERSION",
+            ARG::UNKNOWN => "UNKNOWN",
+        }
+    }
 }
 
 impl fmt::Display for ARG {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.as_str())
+        write!(f, "{}", self.debug())
     }
 }
 
 lazy_static! {
     static ref ARGS: HashMap<&'static str, ARG> = {
-        let mut flags = HashMap::new();
-        flags.insert(ARG::API.as_str(), ARG::API);
-        flags.insert(ARG::CONFIG.as_str(), ARG::CONFIG);
-        flags.insert(ARG::DEBUG.as_str(), ARG::DEBUG);
-        flags.insert(ARG::DIRECTORY.as_str(), ARG::DIRECTORY);
-        flags.insert(ARG::ENVIRONMENT.as_str(), ARG::ENVIRONMENT);
-        flags.insert(ARG::EXECUTE.as_str(), ARG::EXECUTE);
-        flags.insert(ARG::FILES.as_str(), ARG::FILES);
-        flags.insert(ARG::HELP.as_str(), ARG::HELP);
-        flags.insert(ARG::PRINT.as_str(), ARG::PRINT);
-        flags.insert(ARG::PROJECT.as_str(), ARG::PROJECT);
-        flags.insert(ARG::REQUIRED.as_str(), ARG::REQUIRED);
-        flags.insert(ARG::SAVE.as_str(), ARG::SAVE);
-        flags.insert(ARG::VERSION.as_str(), ARG::VERSION);
-        flags.insert(ARG::UNKNOWN.as_str(), ARG::UNKNOWN);
-        return flags;
+        return HashMap::from([
+            (ARG::API.as_str(), ARG::API),
+            (ARG::CONFIG.as_str(), ARG::CONFIG),
+            (ARG::DEBUG.as_str(), ARG::DEBUG),
+            (ARG::DIRECTORY.as_str(), ARG::DIRECTORY),
+            (ARG::ENVIRONMENT.as_str(), ARG::ENVIRONMENT),
+            (ARG::EXECUTE.as_str(), ARG::EXECUTE),
+            (ARG::FILES.as_str(), ARG::FILES),
+            (ARG::HELP.as_str(), ARG::HELP),
+            (ARG::PRINT.as_str(), ARG::PRINT),
+            (ARG::PROJECT.as_str(), ARG::PROJECT),
+            (ARG::REQUIRED.as_str(), ARG::REQUIRED),
+            (ARG::SAVE.as_str(), ARG::SAVE),
+            (ARG::VERSION.as_str(), ARG::VERSION),
+            (ARG::UNKNOWN.as_str(), ARG::UNKNOWN),
+        ]);
     };
 }
 
@@ -94,7 +113,7 @@ pub fn parse<'a>(options: &mut OptionsType<'a>) {
             Some(ARG::REQUIRED) => println!("{}", ARG::REQUIRED),
             Some(ARG::SAVE) => options.save = true,
             Some(ARG::VERSION) => Logger::print_version_and_exit(),
-            Some(ARG::UNKNOWN) | None => println!("UNKNOWN {}", flag),
+            Some(ARG::UNKNOWN) | None => println!("{} {}", ARG::UNKNOWN, flag),
         }
 
         i += 1;
