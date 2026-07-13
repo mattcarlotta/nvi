298d725e7f62129f911c0204c49b1c13ec03dc61
Matt Carlotta <matt@mattcarlotta.com>
2026-07-13 08:30:27 -0700

    fix: update magic hash number

--- src/hash.c
+++ src/hash.c
@@ -1,11 +1,11 @@
 #include "hash.h"
 
-#define FNV_OFFSET 1469598103934665603ULL
+#define FNV_OFFSET 14695981039346656037ULL
 #define FNV_PRIME 1099511628211ULL
 
 // This is FNV-1a (Fowler-Noll-Vo), a non-cryptographic hash function.
 // The two magic constants are specified by the algorithm:
-// Magic Offset basis: 1469598103934665603 (decimal) -- this is the seed
+// Magic Offset basis: 14695981039346656037 (decimal) -- this is the seed
 // Magic Prime: 1099511628211 -- chosen for good avalanche properties over 64-bit integers
 uint64_t fnv1a(const char *key, size_t len) {
     uint64_t h = FNV_OFFSET;
